09b22ad67c769ff054867c21b58dfbd3d377f947
Aditya More <pvt.meadi@gmail.com>
2025-12-22 00:48:33 +0530

    Greatest Of 3 Numbers

---
+++ Basics/greates of 3 nums.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a, b, c;
+    printf("Enter 3 numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a > b && a>c)
+        printf("Largest number = %d", a);
+    else
+        {
+        	if (b>a && b>c)
+        	printf("Largest number = %d", b);
+        	else
+        	printf("Largest number = %d", c);
+		}
+
+    return 0;
+    getch ();
+}
+
